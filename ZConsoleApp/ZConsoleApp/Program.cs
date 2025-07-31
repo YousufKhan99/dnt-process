5333cc007b7d8faaed0ebce548ef6b39a31c4c67
YousufKhan99 <Yousuf.Khan@danatonline.com>
2025-07-31 13:50:44 +0500

    App Architecture

---
+++ ZConsoleApp/ZConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App Architecture");
+        }
+    }
+}
